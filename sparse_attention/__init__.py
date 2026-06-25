a0865be4ed74b6f7ae9253d60fcfec244fae308d
Prachi kushwaha <prachiku673@gmail.com>
2026-06-25 15:46:36 +0000

    chnges in test and readme

--- sparse_attention/__init__.py
+++ sparse_attention/__init__.py
@@ -1,8 +1,11 @@
 from .cp import cp_boundary_exchange
 from .indexing import build_topk
+from .cp import cp_globalize_indices
+from .cp import cp_localize_indices
 
 __all__ = [
     "cp_boundary_exchange",
     "build_topk",
-
+    "cp_globalize_indices",
+    "cp_localize_indices"
 ]
--- sparse_attention/kv_ops/kernels/__init__.py
+++ sparse_attention/kv_ops/kernels/__init__.py
@@ -1,5 +1,5 @@
-from cp_gather_kv import cp_gather_kv_kernel
-from gather_kv import gather_kv_kernel
+from .cp_gather_kv import cp_gather_kv_kernel
+from .gather_kv import gather_kv_kernel
 
 __all__ = [
     "cp_gather_kv_kernel",
